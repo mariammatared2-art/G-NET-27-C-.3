9f43d318a7de43d0a43c7ca3c1927220011c5588
mariam matared <mariammatared2@gmail.com>
2026-02-23 20:09:57 +0200

    Question 1

--- Session 3/Program.cs
+++ Session 3/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            #region QUESTION 1
+            //    1.What will this print and explain what happens ?
+
+            //     double d = 9.99;
+            //    int x = (int)d;
+            //    Console.WriteLine(x);
+
+            //Output: 9
+            //     Explanation: Casting a double to an int.
+
+            #endregion
         }
     }
 }
